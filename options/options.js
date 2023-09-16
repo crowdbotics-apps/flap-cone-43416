ce25705c7fbfdba891c9edf347468fa3a2399909
Crowdbotics <team@crowdbotics.com>
2023-09-16 11:46:03 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "flap-cone-43416",
+  url: "https://flap-cone-43416.botics.co",
+  api: "https://flap-cone-43416.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
